ecce96b40239c3f5c725e3b5772bb3c02b5e36b5
shmizuyui <yuihana760@gmail.com>
2022-06-06 16:18:41 +0900

    レッスン詳細作成・rspec追加

--- app/controllers/api/v1/user/lessons_controller.rb
+++ app/controllers/api/v1/user/lessons_controller.rb
@@ -7,7 +7,12 @@ class Api::V1::User::LessonsController < ApplicationController
 
   def show
     form = Api::V1::User::Lessons::ShowForm.new(params)
-    response = form.show
-    render json: response
+
+    if form.valid?
+      response = form.show
+      render json: response
+    else
+      render json: ErrorResponse.base_response(I18n.t('errors.bad_request'), STATUS_BAD_REQUEST)
+    end
   end
 end
