28ce233d6d2550d5dc99c9d6eb236ba2f47ba1d7
shmizuyui <yuihana760@gmail.com>
2022-06-17 13:20:07 +0900

    ページネーションのrspec変更

--- spec/requests/api/v1/user/lessons_spec.rb
+++ spec/requests/api/v1/user/lessons_spec.rb
@@ -1,48 +1,50 @@
 require 'rails_helper'
 
-describe 'GET /api/v1/user/lessons' do
-  context '正常系' do
+describe 'LessonAPI' do
+  describe 'GET /api/v1/user/lessons' do
+    context '正常系' do
+      let(:lesson) { create(:lesson) }
+      let(:params) { { page: 1 } }
+
+      before do
+        lesson
+        get api_v1_user_lessons_path, params:
+      end
+
+      it { expect(response).to have_http_status :ok }
+      it { expect(JSON.parse(response.body)['data']['lessons'][0]['id']).to eq lesson.id }
+      it { expect(JSON.parse(response.body)['data']['lessons'][0]['title']).to eq lesson.title }
+      it { expect(JSON.parse(response.body)['data']['lessons'][0]['category']).to eq lesson.category }
+      it { expect(JSON.parse(response.body)['data']['lessons'][0]['price']).to eq lesson.price }
+      it { expect(JSON.parse(response.body)['data']['lessons'][0]['time']).to eq lesson.time }
+    end
+  end
+
+  describe 'GET /api/v1/user/lessons/:id' do
     let(:lesson) { create(:lesson) }
-    let(:params) { { page: 1 } }
 
-    before do
-      lesson
-      get api_v1_user_lessons_path, params:
+    context '正常系' do
+      before do
+        get api_v1_user_lesson_path(lesson.id)
+      end
+
+      it { expect(response).to have_http_status :ok }
+      it { expect(JSON.parse(response.body)['data']['lesson']['id']).to eq lesson.id }
+      it { expect(JSON.parse(response.body)['data']['lesson']['title']).to eq lesson.title }
+      it { expect(JSON.parse(response.body)['data']['lesson']['category']).to eq lesson.category }
+      it { expect(JSON.parse(response.body)['data']['lesson']['price']).to eq lesson.price }
+      it { expect(JSON.parse(response.body)['data']['lesson']['time']).to eq lesson.time }
     end
 
-    it { expect(response).to have_http_status :ok }
-    it { expect(JSON.parse(response.body)['data']['lessons'][0]['id']).to eq lesson.id }
-    it { expect(JSON.parse(response.body)['data']['lessons'][0]['title']).to eq lesson.title }
-    it { expect(JSON.parse(response.body)['data']['lessons'][0]['category']).to eq lesson.category }
-    it { expect(JSON.parse(response.body)['data']['lessons'][0]['price']).to eq lesson.price }
-    it { expect(JSON.parse(response.body)['data']['lessons'][0]['time']).to eq lesson.time }
-  end
-end
+    context '異常系' do
+      let(:not_exist_lesson_id) { lesson.id + 1 }
 
-describe 'GET /api/v1/user/lessons/:id' do
-  let(:lesson) { create(:lesson) }
+      before do
+        get api_v1_user_lesson_path(not_exist_lesson_id)
+      end
 
-  context '正常系' do
-    before do
-      get api_v1_user_lesson_path(lesson.id)
+      it { expect(JSON.parse(response.body)['error']).to eq 'レッスンが見つかりません' }
+      it { expect(JSON.parse(response.body)['status']).to eq 404 }
     end
-
-    it { expect(response).to have_http_status :ok }
-    it { expect(JSON.parse(response.body)['data']['lesson']['id']).to eq lesson.id }
-    it { expect(JSON.parse(response.body)['data']['lesson']['title']).to eq lesson.title }
-    it { expect(JSON.parse(response.body)['data']['lesson']['category']).to eq lesson.category }
-    it { expect(JSON.parse(response.body)['data']['lesson']['price']).to eq lesson.price }
-    it { expect(JSON.parse(response.body)['data']['lesson']['time']).to eq lesson.time }
-  end
-
-  context '異常系' do
-    let(:not_exist_lesson_id) { lesson.id + 1 }
-
-    before do
-      get api_v1_user_lesson_path(not_exist_lesson_id)
-    end
-
-    it { expect(JSON.parse(response.body)['error']).to eq 'レッスンが見つかりません' }
-    it { expect(JSON.parse(response.body)['status']).to eq 404 }
   end
 end
