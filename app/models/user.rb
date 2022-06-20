0f5241dff400c0d35539c43e15968cee7bef240d
shimizzuu <yuta6113@gmail.com>
2022-06-21 00:11:48 +0900

    新規ユーザー登録API作成

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,8 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   include DeviseTokenAuth::Concerns::User
+
+  validates :name, :gender, :email, presence: true
+
+  enum gender: { male: 1, female: 2 }
 end
