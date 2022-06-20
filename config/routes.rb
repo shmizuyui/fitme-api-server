cad5b9770272c6ff46f96f081589743e2eea8ce9
shimizzuu <yuta6113@gmail.com>
2022-06-21 00:11:48 +0900

    新規ユーザー登録API作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,13 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth'
   namespace :api do
     namespace :v1 do
       namespace :user do
+        mount_devise_token_auth_for 'User', at: 'auth', controllers: {
+          registrations: 'api/v1/user/auth/registrations'
+        }
+        namespace :auth do
+          resources :sessions, only: %i[index]
+        end
         resources :lessons, only: %i[index show]
         resources :trainers, only: %i[index show]
         namespace :lesson do
