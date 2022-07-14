dab1f1940d9f3e3945f566b2a10b53e0451314cd
shmizuyui <yuihana760@gmail.com>
2022-07-14 18:34:36 +0900

    ログイン機能

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+  include DeviseTokenAuth::Concerns::User
+
+  validates :name, :name_kana, :email, :gender, presence: true
+
+  enum gender: { male: 1, female: 2 }
+end
