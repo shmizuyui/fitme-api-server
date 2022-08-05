d3916cb9eae055ff01018685042874ddfd8eb978
shmizuyui <yuihana760@gmail.com>
2022-08-05 18:12:35 +0900

    編集機能テスト

--- spec/requests/api/v1/user/auth/registrations_spec.rb
+++ spec/requests/api/v1/user/auth/registrations_spec.rb
@@ -24,4 +24,31 @@ RSpec.describe 'Api::V1::User::Auth::Registrations', type: :request do
       it { expect(JSON.parse(response.body)['errors']['full_messages']).to eq ['パスワード（確認）とパスワードの入力が一致しません'] }
     end
   end
+
+  describe 'PUT /api/v1/user/auth' do
+    let(:user) { create(:user) }
+
+    before do
+      put api_v1_user_user_registration_path(user), params:, headers: headers
+    end
+    
+    context '編集ができるとき' do
+      let(:params) { {name:'名前', name_kana:'ナマエ', email:'email', password: 'passpass', password_confirmation:'passpass'} }
+      let(:headers) {user.create_new_auth_token}
+      
+      it { expect(response).to have_http_status :ok }
+      it { expect(JSON.parse(response.body)['data']['name']).to eq '名前' }
+
+
+      # it 'データが更新される' do
+      #   subject
+      #   user.reload
+      #   expect(user.name).to eq '名前'
+      #   expect(user.name_kana).to eq 'ナマエ'
+      #   expect(user.email).to eq 'email'
+      #   expect(user.password).to eq 'passpass'
+      #   expect(user.password_confirmation).to eq 'passpass'
+      # end
+    end
+  end
 end
