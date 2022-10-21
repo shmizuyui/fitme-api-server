7dee4c187589e8828f61d7fa344418ee32876754
shmizuyui <yuihana760@gmail.com>
2022-10-21 22:06:09 +0900

    URLの設定

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "*"
+    origins "*", /fitme-web-front\.herokuapp\.com/
 
     resource "*",
       headers: :any,
